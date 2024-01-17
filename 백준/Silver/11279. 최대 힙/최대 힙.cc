3966e2ab4e497558e8aca52724cd191d6d77ebf3
GyeongminY <142128469+GyeongminY@users.noreply.github.com>
2024-01-17 14:26:01 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <queue>
+using namespace std;
+
+class cmp {
+public:
+	bool operator() (int x, int y) {
+		return x < y;
+	}
+};
+priority_queue<int, vector<int>, cmp> pq;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int k;
+		cin >> k;
+		if (k == 0) {
+			if (!pq.empty()) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else cout << "0\n";
+		}
+		else {
+			pq.push(k);
+		}
+	}
+}
