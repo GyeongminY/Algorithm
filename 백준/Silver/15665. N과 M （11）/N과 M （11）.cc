5e1ac9d3859d3f4900a09953fe5a2a48727cf721
GyeongminY <142128469+GyeongminY@users.noreply.github.com>
2024-02-19 21:22:47 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int> v;
+int arr[9];
+//bool isused[10002];
+
+void func(int k) { //st는 arr[0]에 들어갈 수의 v에서의 인덱스, k는 arr의 k번째에 들어갈 차례라는 뜻.
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	int temp = 0;
+	for (int i = 0; i < n; i++) {
+		if (temp != v[i]) {
+			arr[k] = v[i];
+			temp = arr[k];
+			func(k + 1);
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	func(0);
+}
