7674185dbb5bb59d742c554963a03b51d7a60728
GyeongminY <142128469+GyeongminY@users.noreply.github.com>
2024-02-19 16:57:24 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int> v;
+int arr[9];
+bool isused[10001];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (k != 0 && v[i] < arr[k - 1]) continue;
+		arr[k] = v[i];
+		func(k + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	int a;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	func(0);
+}
