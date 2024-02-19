730e42b4ab59611f274a18583cbae0ba6452c75f
GyeongminY <142128469+GyeongminY@users.noreply.github.com>
2024-02-19 16:15:32 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[9];
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		if (k != 0 && arr[k - 1] > i) continue;
+		arr[k] = i;
+		func(k + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	cin >> n >> m;
+	func(0);
+}
